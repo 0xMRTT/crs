b46eb9456791258b542ed12a8427aa0c02f3bc4c
0xMRTT <0xMRTT@tuta.io>
2022-06-16 17:06:13 +0200

    Add print data for debuging

--- src/main.rs
+++ src/main.rs
@@ -294,6 +294,8 @@ fn ask_user(template_json_path: String) {
             let result = Text::new(question.as_str()).with_placeholder(placeholder).with_default(default).with_help_message(description).prompt();
             data.insert(key.to_string(), Json::String(result.unwrap()));
         }
+
+        println!("{:#?}", data);
     }
 }
 
